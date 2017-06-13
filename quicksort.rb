7f91368bc2114f65789bf5842c7fed583cc7a16b
Jon Elofson <jonathan.elofson@gmail.com>
2017-06-13 11:55:55 -0400

    add quicksort in place

--- quicksort.rb
+++ quicksort.rb
@@ -12,3 +12,18 @@ def quicksort(arr)
   end
   return quicksort(less_than_or_eq) + [pivot] + quicksort(greater_than)
 end
+
+
+def quicksort_ip(arr)
+  return arr if arr.length() <= 1
+  pivot_idx = 0
+  pivot_val = arr[0]
+  (1...arr.length).each do |idx|
+    if arr[idx] < pivot_val
+      arr[idx], arr[pivot_idx] = arr[pivot_idx], arr[idx]
+      pivot_idx += 1
+      idx += 1
+    end
+  end
+  return quicksort_ip(arr[0...pivot_idx]) + [pivot_val] + quicksort_ip(arr[pivot_idx+1..-1])
+end
